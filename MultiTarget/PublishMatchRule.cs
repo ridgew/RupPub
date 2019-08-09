39f4bb9c111d3b7e0be8c72eb057de293ca0ce6b
Ridge <email@qinjun.wang>
2019-08-09 17:53:09 +0800

    add mtf draft

---
+++ MultiTarget/PublishMatchRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RupPub
+{
+    public class PublishMatchRule : MatchRule
+    {
+        public override bool IsMatch(FileInfo fi)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MultiTargetPub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RupPub
+{
+    public class MultiTargetPub : PackagePub
+    {
+        public MultiTargetPub(string pubRulefile)
+        {
+
+        }
+
+        public override bool Pub(DirectoryInfo baseDi, FileInfo[] allFiles)
+        {
+            return false;
+        }
+    }
+}
